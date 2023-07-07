408463d979debfd62ec651a75dc2bdda0567217d
BitterChoco <kimberly.moriles@gmail.com>
2023-07-07 13:38:31 +0800

    Collision detection

--- app/src/main/java/com/wasabi/electron/Game.java
+++ app/src/main/java/com/wasabi/electron/Game.java
@@ -10,8 +10,13 @@ import android.view.SurfaceView;
 import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
 
+import com.wasabi.electron.object.Circle;
 import com.wasabi.electron.object.Enemy;
 import com.wasabi.electron.object.Player;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Game manages all objects in the game and is responsible for updating all states and render all
@@ -20,8 +25,9 @@ import com.wasabi.electron.object.Player;
 public class Game extends SurfaceView implements SurfaceHolder.Callback {
     private final Joystick joystick;
     private final Player player;
-    private final Enemy enemy;
+    //private final Enemy enemy;
     private GameLoop gameLoop;
+    private List<Enemy> enemyList = new ArrayList<Enemy>();
 
     public Game(Context context) {
         super(context);
@@ -36,7 +42,7 @@ public class Game extends SurfaceView implements SurfaceHolder.Callback {
         // Initialize game objects
         joystick = new Joystick(275, 700, 70, 40);
         player = new Player(getContext(), joystick, 2 * 500, 500, 30);
-        enemy = new Enemy(getContext(), player, 500, 200, 30);
+        //enemy = new Enemy(getContext(), player, 500, 200, 30);
 
         setFocusable(true);
     }
@@ -88,7 +94,9 @@ public class Game extends SurfaceView implements SurfaceHolder.Callback {
 
         joystick.draw(canvas);
         player.draw(canvas);
-        enemy.draw(canvas);
+        for (Enemy enemy : enemyList) {
+            enemy.draw(canvas);
+        }
     }
 
     public void drawUPS(Canvas canvas) {
@@ -113,6 +121,23 @@ public class Game extends SurfaceView implements SurfaceHolder.Callback {
         // Update game state
         joystick.update();
         player.update();
-        enemy.update();
+
+        // Spawn enemy if it is time to spawn new enemies
+        if (Enemy.readyToSpawn()) {
+            enemyList.add(new Enemy(getContext(), player));
+        }
+        // Update each state of each enemy
+        for (Enemy enemy : enemyList) {
+            enemy.update();
+        }
+
+        // Iterate through enemies and check for collision between each enemy and the player
+        Iterator<Enemy> iteratorEnemy = enemyList.iterator();
+        while (iteratorEnemy.hasNext()) {
+            if (Circle.isColliding(iteratorEnemy.next(), player)) {
+                // Remove enemy if it collides with the player
+                iteratorEnemy.remove();
+            }
+        }
     }
 }
